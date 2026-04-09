c405ce339d19ef3d90a2729a782e46bbd4e91031
Samyukta <samyukta2628@gmail.com>
2026-04-09 10:45:11 +0530

    h

---
+++ AB_or_C.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define vi vector<ll>
+#define vvi vector<vector<ll>>
+#define maxheap priority_queue<ll>
+#define minheap priority_queue<ll, vi, greater<ll>>
+
+#define sortv(v) sort(v.begin(), v.end())
+#define sortvr(v) sort(v.begin(), v.end(), greater<>())
+
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define nl cout << endl
+
+#define def1(n) \
+    ll n;       \
+    cin >> n
+#define def2(a, b) \
+    ll a, b;       \
+    cin >> a >> b
+#define def3(a, b, c) \
+    ll a, b, c;       \
+    cin >> a >> b >> c
+#define def4(a, b, c, d) \
+    ll a, b, c, d;       \
+    cin >> a >> b >> c >> d
+#define def5(a, b, c, d, e) \
+    ll a, b, c, d, e;       \
+    cin >> a >> b >> c >> d >> e
+
+#define out(n) cout << n
+#define outs(n) cout << n << " "
+#define outl(n) cout << n << endl
+#define out2(a, b) cout << a << " " << b << endl
+#define out3(a, b, c) cout << a << " " << b << " " << c << endl
+#define out4(a, b, c, d) cout << a << " " << b << " " << c << " " << d << endl
+#define out5(a, b, c, d, e) cout << a << " " << b << " " << c << " " << d << " " << e << endl
+
+#define inv(v, n)              \
+    vi v(n);                   \
+    for (ll i = 0; i < n; i++) \
+        cin >> v[i];
+
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+ll power(ll x, ll y)
+{
+    ll res = 1;
+    if (x == 0)
+        return 0;
+    while (y > 0)
+    {
+        if (y & 1)
+            res = (res * x);
+        y = y >> 1;
+        x = (x * x);
+    }
+    return res;
+}
+
+void solve()
+{
+    def1(n);
+    while(n>0){
+
+    ll w = max({a[1], b[1], c[1]});
+    ll x = max({a[0], b[0], c[0]});
+    ll y = max({a[2], b[2], c[2]});
+    ll z = max({a[3], b[3], c[3]});
+    ll p = min({w, x, y, z});
+    ll q = max({w, x, y, z});
+    outl(q - p);}
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t = 1;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
